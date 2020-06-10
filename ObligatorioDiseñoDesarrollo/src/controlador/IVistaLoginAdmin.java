ffa2c3d78dfd6cb3e1bf99499905f6adf030d9fd
Fernando <fernandoperez1902@gmail.com>
2020-06-10 01:10:33 -0300

    Sin vista de bienvenida
    Porque no era importante un controlador y una vista expresamente para un mensaje

--- ObligatorioDiseñoDesarrollo/src/controlador/IVistaLoginAdmin.java
+++ ObligatorioDiseñoDesarrollo/src/controlador/IVistaLoginAdmin.java
@@ -8,6 +8,8 @@ public interface IVistaLoginAdmin {
     public void mostrarError(String login_incorrecto);
     
     public void mostrarProximaVista(Administrador admin);
+    
+    public void mostrarBienvenida(Administrador admin);
 
     public void cerrar();
 
