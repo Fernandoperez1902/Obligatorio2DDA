af98d20835ff8f8f8effc480f98e3006742d6ebb
marrietap <51885600+marrietap@users.noreply.github.com>
2020-06-10 03:58:04 -0300

    arreglo

--- ObligatorioDiseñoDesarrollo/src/modelo/Apuesta.java
+++ ObligatorioDiseñoDesarrollo/src/modelo/Apuesta.java
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:ObligatorioDiseñoDesarrollo/src/modelo/ModalidadApuesta.java
->>>>>>> Submaster
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-<<<<<<< HEAD
-=======
+
 import java.util.ArrayList;
 
->>>>>>> Submaster
-/**
- *
- * @author Mauro
- */
-<<<<<<< HEAD
+
 public class Apuesta {
     
     private float monto;
@@ -30,34 +16,6 @@ public class Apuesta {
         this.participante = participante;
         this.monto = monto;
     }
-    
-    
-    
-}
-=======
-public abstract interface ModalidadApuesta {
-    public void calcular();
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package modelo;
-
-/**
- *
- * @author Mauro
- */
-public class Apuesta {
-    
-    private float monto;
-    private Jugador jugador;
-    
-    
-    
+   
     
 }
->>>>>>> Submaster:ObligatorioDiseñoDesarrollo/src/modelo/Apuesta.java
->>>>>>> Submaster
--- ObligatorioDiseñoDesarrollo/src/modelo/ModalidadApuesta.java
+++ ObligatorioDiseñoDesarrollo/src/modelo/ModalidadApuesta.java
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:ObligatorioDiseñoDesarrollo/src/modelo/ModalidadApuesta.java
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
 import java.util.ArrayList;
@@ -15,25 +10,3 @@ import java.util.ArrayList;
 public abstract interface ModalidadApuesta {
     public void calcular();
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package modelo;
-
-/**
- *
- * @author Mauro
- */
-public class Apuesta {
-    
-    private float monto;
-    private Jugador jugador;
-    
-    
-    
-    
-}
->>>>>>> Submaster:ObligatorioDiseñoDesarrollo/src/modelo/Apuesta.java
