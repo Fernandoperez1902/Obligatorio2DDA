77476aa1be96560fc13c81cb7d80d897b77d52df
Fernando <fernandoperez1902@gmail.com>
2020-06-10 01:17:52 -0300

    Update ControladorMenuAdministrador.java

--- ObligatorioDiseñoDesarrollo/src/controlador/ControladorMenuAdministrador.java
+++ ObligatorioDiseñoDesarrollo/src/controlador/ControladorMenuAdministrador.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controlador;
 
-/**
- *
- * @author ferna
- */
+import logica.Administrador;
+
 public class ControladorMenuAdministrador {
+  
+    private Administrador modelo;
+    private IVistaMenuAdministrador vista;
+    
+    public ControladorMenuAdministrador(Administrador modelo, IVistaMenuAdministrador vista){
+        this.modelo = modelo;
+        this.vista = vista;
+    }
     
 }
